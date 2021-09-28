4958eb0dc2a4d63a91679f0ef71da33e9e05be52
ilhamKawe <kawekaweha123@gmail.com>
2021-09-28 18:23:13 +0700

    :sparkles: init comit

---
+++ resources/views/front/includes/script.blade.php
@@ -0,0 +1,3 @@
+<script type="applijewelleryion/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<script src="assets/js/jquery-1.11.1.min.js"></script>
+<script type="text/javascript" src="assets/js/bootstrap.js"></script>
---
+++ resources/views/front/includes/style.blade.php
@@ -0,0 +1,8 @@
+<link href="assets/css/bootstrap.css" rel='stylesheet' type='text/css' />
+<!-- Custom Theme files -->
+<link href="assets/css/style.css" rel='stylesheet' type='text/css' />
+
+<!--webfonts-->
+<link href='//fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Open+Sans:400,300italic,300,600' rel='stylesheet' type='text/css'>
+<!--//webfonts-->
